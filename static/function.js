d3e2d8354729ca9e21ecbfc809aa32087cf3a035
leandroluizpereira <leandro.2306@hotmail.com>
2021-07-02 14:06:26 -0300

    texto

--- static/function.js
+++ static/function.js
@@ -1,13 +1,15 @@
 function btnMais(){
     alert("clicou +");
 
-    document.getElementById("text).innerHTML="+";
+    document.getElementById("text").innerHTML="+";
 
 }
 
 
 function resultado(){
-    alert("resultado");
+    
+ 
+    alert("resultado  ");
     var  resultado = document.getElementById("text");
     document.getElementById("text").eval(resultado);
 }
